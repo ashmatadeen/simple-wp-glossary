9f228ad1a6e984a37ec6f9ac9df65f7bdb2da69c
Webblerock <Webblerock@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-27 16:12:33 +0000

    basic css and js for glossary item tooltip
    git-svn-id: http://plugins.svn.wordpress.org/simple-wp-glossary/trunk@1232304 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/simple-wp-glossary.js
@@ -0,0 +1,40 @@
+$(function(){
+  $('dfn').each(function(){
+    var timer;
+    var thisTooltip = $(this);
+    var theTerm = thisTooltip.html();
+    var theDefinition = thisTooltip.attr('title');
+    var tooltipTemplate =
+        '<span class="dfnTooltip">' +
+          '<span class="dfnTooltipTerm">' + theTerm + '</span>' +
+          '<span class="dfnTooltipDefinition">' + theDefinition + '</span>' +
+        '</span>';
+    
+    thisTooltip.attr('title', '')
+      .addClass('hasTooltip')
+      .append(tooltipTemplate)
+      .hover(
+        function(e){
+          if (timer) { clearTimeout(timer); timer = null; }
+          
+          timer = setTimeout(function(){
+            var position = thisTooltip.position();
+            console.log(position.top);
+            console.log(position.left);
+            thisTooltip
+              .children('.dfnTooltip')
+              .css({ 'top': position.top + 10, 'left': position.left + 20 })
+              .fadeIn(200)
+          }, 200);
+        },
+        function(){
+          if (timer) { clearTimeout(timer); timer = null; }
+          
+          timer = setTimeout(function(){
+            thisTooltip.children('.dfnTooltip').fadeOut();
+          }, 200);
+        }
+      )
+      .children('.dfnTooltip').hide();
+  });
+});
