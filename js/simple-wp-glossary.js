95e811a3b079ca82a3d6c7ef8d480e276746a3d0
Webblerock <Webblerock@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-27 16:13:07 +0000

    jquery and style tweaks
    git-svn-id: http://plugins.svn.wordpress.org/simple-wp-glossary/trunk@1232307 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/simple-wp-glossary.js
+++ js/simple-wp-glossary.js
@@ -1,40 +1,42 @@
-$(function(){
-  $('dfn').each(function(){
-    var timer;
-    var thisTooltip = $(this);
-    var theTerm = thisTooltip.html();
-    var theDefinition = thisTooltip.attr('title');
-    var tooltipTemplate =
-        '<span class="dfnTooltip">' +
-          '<span class="dfnTooltipTerm">' + theTerm + '</span>' +
-          '<span class="dfnTooltipDefinition">' + theDefinition + '</span>' +
-        '</span>';
-    
-    thisTooltip.attr('title', '')
-      .addClass('hasTooltip')
-      .append(tooltipTemplate)
-      .hover(
-        function(e){
-          if (timer) { clearTimeout(timer); timer = null; }
-          
-          timer = setTimeout(function(){
-            var position = thisTooltip.position();
-            console.log(position.top);
-            console.log(position.left);
-            thisTooltip
-              .children('.dfnTooltip')
-              .css({ 'top': position.top + 10, 'left': position.left + 20 })
-              .fadeIn(200)
-          }, 200);
-        },
-        function(){
-          if (timer) { clearTimeout(timer); timer = null; }
-          
-          timer = setTimeout(function(){
-            thisTooltip.children('.dfnTooltip').fadeOut();
-          }, 200);
-        }
-      )
-      .children('.dfnTooltip').hide();
+jQuery( document ).ready(function() {
+  jQuery(function(){
+    jQuery('dfn').each(function(){
+      var timer;
+      var thisTooltip = jQuery(this);
+      var theTerm = thisTooltip.html();
+      var theDefinition = thisTooltip.attr('title');
+      var tooltipTemplate =
+          '<span class="dfnTooltip">' +
+            '<span class="dfnTooltipTerm">' + theTerm + '</span>' +
+            '<span class="dfnTooltipDefinition">' + theDefinition + '</span>' +
+          '</span>';
+      
+      thisTooltip.attr('title', '')
+        .addClass('hasTooltip')
+        .append(tooltipTemplate)
+        .hover(
+          function(e){
+            if (timer) { clearTimeout(timer); timer = null; }
+            
+            timer = setTimeout(function(){
+              var position = thisTooltip.position();
+              console.log(position.top);
+              console.log(position.left);
+              thisTooltip
+                .children('.dfnTooltip')
+                .css({ 'top': position.top + 22, 'left': position.left + 22 })
+                .fadeIn(200)
+            }, 200);
+          },
+          function(){
+            if (timer) { clearTimeout(timer); timer = null; }
+            
+            timer = setTimeout(function(){
+              thisTooltip.children('.dfnTooltip').fadeOut();
+            }, 200);
+          }
+        )
+        .children('.dfnTooltip').hide();
+    });
   });
 });
